424dc65b72dd41cd5cf0ab3463f6e5916a8fbc3d
yonicb <yonicb20@gmail.com>
2020-02-09 13:03:14 -0500

    ci: 🎡 Aggregate logic for iva tax

--- main.cpp
+++ main.cpp
@@ -4,5 +4,27 @@ using namespace std;
 
 int main()
 {
+   // Entered value
+   int value;
+
+   // https://en.wikipedia.org/wiki/Value-added_tax
+   // Tax for PERU = 18%
+   int iva = 18;
+
+   // (value * iva) / 100
+   int ivaResult;
+
+   // value - ivaResult
+   int result;
+
+   cout << "Enter the value: ";
+   cin >> value;
+
+   ivaResult = (value * iva) / 100;
+   result = value - ivaResult;
+
+   cout << "The VAT charged is: " << ivaResult << endl;
+   cout << "Value applied with VAT: " << result << endl;
+
    return 0;
 }
